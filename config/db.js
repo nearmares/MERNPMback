6ff4b7601fa54e8a769678e5ed232ca8106d2ae3
Nestor <nestor.marcano6@gmail.com>
2021-09-30 14:56:23 -0300

    .

--- config/db.js
+++ config/db.js
@@ -1,22 +1,15 @@
 const mongoose = require("mongoose");
 require("dotenv").config({path: "variables.env"});
  
-const conectarDB = () => {
-  // try {
-  //   await mongoose.connect( process.env.DB_MONGO);
-  //   console.log("Data Base connected");
+const conectarDB = async () => {
+  try {
+    await mongoose.connect( process.env.DB_MONGO);
+    console.log("Data Base connected");
 
-  // } catch (error) {
-  //   console.log(error)
-  //   process.exit(1) //detener la app
-  // };
-  mongoose
-     .connect( process.env.DB_MONGO)
-     .then(() => console.log( 'Database Connected' ))
-     .catch(err => console.log( err ));
-
+  } catch (error) {
+    console.log(error)
+   process.exit(1) //detener la app
+  };
 };
-
-
 
 module.exports = conectarDB;
